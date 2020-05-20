d106f8472b14fbc20652dac64f92d2ab54508eb3
EatYourSoup7 <r.kondratowicz1@gmail.com>
2020-05-19 19:30:54 -0500

    Built SearchMovies Component

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './style.css';
+import SearchMovies from "./searchMovies";
 
 class Main extends React.Component {
   render() {
     return (
       <div className="container">
         <h1 className="title">React Movie Search</h1>
+        <SearchMovies />
       </div>
     );
   }
---
+++ src/searchMovies.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function SearchMovies() {
+  return (
+    <form className="form">
+      <label htmlFor="query" className="label">Movie Name</label>
+      <input type="text" name="query" placeholder="Jurassic Park" />
+      <button className="button" type="button">Search</button>
+    </form>
+  )
+}
